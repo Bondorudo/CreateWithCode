dfa4a69e001d1a570c995aed3aa4076ed6bc41a2
Bondorudo <thomas.aaltonen@hotmail.com>
2020-08-20 10:28:02 +0300

    CreateWithCode Lab

---
+++ CreateWithCodeMyGame/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] enemies;
+    public GameObject powerup;
+
+    private float zEnemySpawn = 12;
+    private float xSpawnRange = 13;
+    private float zPowerupRange = 5;
+    private float ySpawn = 0.75f;
+
+    private float powerupSpawnTime = 5;
+    private float enemySpawnTime = 1;
+    private float startDelay = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomEnemy", startDelay, enemySpawnTime);
+        InvokeRepeating("SpawnPowerup", startDelay, powerupSpawnTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnRandomEnemy()
+    {
+        float randomX = Random.Range(-xSpawnRange, xSpawnRange);
+        int randomIndex = Random.Range(0, enemies.Length);
+
+        Vector3 spawnPos = new Vector3(randomX, ySpawn, zEnemySpawn);
+
+        Instantiate(enemies[randomIndex], spawnPos, enemies[randomIndex].gameObject.transform.rotation);
+    }
+
+    void SpawnPowerup()
+    {
+        float randomX = Random.Range(-xSpawnRange, xSpawnRange);
+        float randomZ = Random.Range(-zPowerupRange, zPowerupRange);
+
+        Vector3 spawnPos = new Vector3(randomX, ySpawn, randomZ);
+
+        Instantiate(powerup, spawnPos, powerup.gameObject.transform.rotation);
+    }
+}
